5c16a2a709a7b2410cc7bca1783d8b434ac866c2
Richard <charichard09@gmail.com>
2022-11-04 09:59:46 -0700

    add code to pass TDD test3 Pizza.totalCost()

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ function Pizza (sizeSelect, cheeseBoolean, meatArray, nonMeatArray, sauceSelect)
   this.cost = 0;
 }
 
-// Test2: "It should check for true and add 1 to pizza1 cost"
+// Test3: "It should check for ["sausage", "pepperoni"] and add 2 to pizza1 cost"
 // Code: 
 // const pizza1 = new Pizza("medium", true, ["sausage", "pepperoni"], ["jalapeno", "onion"], "marinara");
 // pizza1.totalCost();
 // Expected Output:
-// pizza1.cost === 14
+// pizza1.cost === 16
 
 Pizza.prototype.totalCost = function () {
   switch (this.size) {
@@ -35,7 +35,24 @@ Pizza.prototype.totalCost = function () {
     this.cost += 1;
   }
 
-  
+  for (const meat of this.meat) {
+    switch (meat) {
+      case ("chicken"):
+      this.cost += 2;
+      break;
+      case ("ham"):
+      this.cost += 1;
+      break; 
+      case ("pepperoni"):
+      this.cost += 1;
+      break; 
+      case ("sausage"):
+      this.cost += 1;
+      break; 
+      default: 
+      this.cost += 0;
+    }
+  }
   
   return this.cost
 }
